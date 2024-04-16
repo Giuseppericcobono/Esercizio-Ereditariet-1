4625286e63a1783b5098b47f8ee00f4c92e2bf41
Giuseppe <giuseppe.riccobono@hotmail.com>
2024-04-16 11:20:46 +0200

    Second Commit

--- src/Gatto.java
+++ src/Gatto.java
@@ -1,7 +1,6 @@
 public class Gatto extends Animale{
     @Override
     public void faiIlVerso() {
-        super.faiIlVerso();
-        System.out.println("Miao");
+        System.out.println("Miao. Sono un gatto.");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Animale animale = new Animale();
         Gatto gatto = new Gatto();
 
+        animale.faiIlVerso();
         gatto.faiIlVerso();
     }
 }
